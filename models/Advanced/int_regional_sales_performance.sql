71635dd85b62f860367c4aa25b382b380e4b1ecd
root <root@ip-10-0-1-127.ec2.internal>
2024-02-26 10:20:19 +0000

    sd

--- models/Advanced/int_regional_sales_performance.sql
+++ models/Advanced/int_regional_sales_performance.sql
@@ -3,7 +3,7 @@ WITH regional_sales AS (
         r_name,
         SUM(o_totalprice) AS total_sales
     FROM {{ ref('stg_orders') }} o
-    JOIN {{ ref('stg_customer') }} c ON o.o_custkey = c.c_custkey
+    JOIN {{ ref('stg_customers') }} c ON o.o_custkey = c.c_custkey
     JOIN {{ ref('stg_nation') }} n ON c.c_nationkey = n.n_nationkey
     JOIN {{ ref('stg_region') }} r ON n.n_regionkey = r.r_regionkey
     GROUP BY r_name
